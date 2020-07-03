ea632bc33fe9f55e4673e548ac2a4541ae95203e
jinho1011 <gino9940@gmail.com>
2020-07-03 15:52:52 +0900

    created base.html

--- jinho1011/ministagram/ministagram/settings.py
+++ jinho1011/ministagram/ministagram/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ministagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'layout')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
