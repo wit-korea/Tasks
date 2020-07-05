6e30dbe4758b93b6494c5e7c2b45924fd4f374c2
jinho1011 <gino9940@gmail.com>
2020-07-05 16:15:32 +0900

    accounts urls

--- jinho1011/ministagram/ministagram/urls.py
+++ jinho1011/ministagram/ministagram/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('photo.urls')),
-    # 가장 기본 주소가 들어오면 photo의 url로 연결
+    path('accounts/', include('accounts.urls')),
 ]
