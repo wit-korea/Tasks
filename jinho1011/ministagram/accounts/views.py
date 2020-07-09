62b27e8cdcb76e6a2697edfc72d4336d4e551c7f
jinho1011 <gino9940@gmail.com>
2020-07-09 15:06:47 +0900

    Create SighUpView in accounts/view.py

--- jinho1011/ministagram/accounts/views.py
+++ jinho1011/ministagram/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import json
+from django.views import View
+from django.http import JsonResponse
+from .models import Account
 
-# Create your views here.
+
+class SignUpView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        Account(
+            email=data['email'],
+            password=data['password']
+        ).save()
+
+        return JsonResponse({'message': '회원가입 완료'}, status=200)
