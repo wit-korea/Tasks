58c20600ff7c0cc921e82a2635312df150f640cd
jinho1011 <gino9940@gmail.com>
2020-07-09 14:45:47 +0900

    Create models.py in accounts app

--- jinho1011/ministagram/accounts/models.py
+++ jinho1011/ministagram/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'accounts'
