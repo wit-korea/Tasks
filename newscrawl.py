82a2215e9dd7fc615c6c99a5899c7c074326b4c8
seoyoung-0 <kdy03013@gmail.com>
2020-08-02 12:24:22 +0900

    Add news link

--- newscrawl.py
+++ newscrawl.py
@@ -13,12 +13,21 @@ title = soup.select(
     '.sub_read_list_box > dl > dt > a'
 )
 
-tag = soup.select('.sub_read_list_box > dl > .sbody> a')
+links = []
+for link in soup.select('.sub_read_list_box > dl > .sbody> a'):
+    if 'href' in link.attrs:
+        conn = link.attrs['href']
+        links.append(conn)
+        
+        
+conn = soup.select('.sub_read_list_box > dl > .sbody > a')
 
-for tag in soup.select('.sub_read_list_box > dl > .sbody> a'):
-    if 'href' in tag.attrs:
-        link = tag.attrs['href']
-        print(link)
+# img = [] 
+# for image in soup.select('.sub_read_list_box > .img_file > p > a > img '):
+#     if 'src' in image.attrs:
+#         img_src = image.attrs['src']
+#         img.append(img_src)
+# print(img)
 
 content = soup.select(
     '.sub_read_list_box > dl > .sbody > a'
@@ -28,16 +37,16 @@ etc = soup.select(
 )
 
 data = []
-for item in zip(title, link, content, etc):
+kfdn = "http://www.kfdn.co.kr" # 식약일보 정책 링크, 기사별 href 만 변경 
+for item in zip(title, links, content, etc):
     data.append(
         {
             'title' : item[0].text,
-            'link' : item[1],
+            'link' : kfdn+item[1],
             'content' : item[2].text,
             'etc' : item[3].text
         }
-    )
+       )
+
 
 print(data)
-
-driver.quit()
